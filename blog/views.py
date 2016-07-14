e82c3bc834d400332f91ac8bcf51347d5f82760b
Joseungwan <wh7923@naver.com>
2016-07-14 19:42:23 +0900

    CRUD기능_완성

--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_delete.html', {'post': post})
