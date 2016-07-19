96446ae7b44fc86ee672c183e71e376ba6559a5b
Joseungwan <wh7923@naver.com>
2016-07-19 12:25:06 +0900

    게시판UI변경

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.photo = request.FILES['photo']
+            post.photo = request.FILES.get('photo',False)
             post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
